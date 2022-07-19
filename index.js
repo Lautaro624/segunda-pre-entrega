61fb297aadcc15cd7a77d5ba858f394404a83fd8
Lautaro <lautarolima624@hotmail.com>
2022-07-18 23:04:43 -0300

    segunda-pre-entrega

---
+++ index.js
@@ -0,0 +1,101 @@
+const restar = (a, b) => a - b;
+const div = (a, b) => a / b;
+const sumar = (a, b) => a + b;
+const multi = (a, b) => a * b;
+let presupuestos = []
+let formulario
+let inputNombre
+let inputAlto
+let inputAncho
+let inputProfundidad
+let inputMaterial
+let contenedorPresupuesto
+class Presupuesto {
+    constructor(nombre, alto, ancho, profundidad, material) {
+        this.nombre =nombre
+        this.alto = alto;
+        this.ancho = ancho;
+        this.profundidad = profundidad;
+        this.material = material;
+    }
+    area = () => (((this.alto * this.ancho) * 0.01) + ((this.alto * this.profundidad) * 0.01))
+    costoTotal = () => { 
+        let valorAMultiplicar = 0
+        switch (this.material){
+            case "MDF laqueado":
+                valorAMultiplicar = 1.5
+                break;
+            case "MDF enchapado":
+                valorAMultiplicar = 2.5
+                break;
+            case "MDF enchapado":
+                valorAMultiplicar = 2.5
+                break;
+            default:
+                valorAMultiplicar = 1
+        }
+        return this.area() * valorAMultiplicar }
+}
+function inicializarElementos (){
+    formulario = document.getElementById("formulario")
+    inputNombre = document.getElementById("nombre")
+    inputAlto = document.getElementById("alto")
+    inputAncho = document.getElementById("ancho")
+    inputProfundidad = document.getElementById("profundidad")
+    inputMaterial = document.getElementById("material")
+    contenedorPresupuesto = document.getElementById("contenedor-presupuesto")
+}
+function inicializarEventos () {
+    formulario.onsubmit = (event) => validarFormulario (event)
+}
+function almacenarPresupuestoLocalStorage() {
+    localStorage.setItem("presupuesto", JSON.stringify(presupuestos))
+}
+function obtenerPresupuestoLocalStorage() {
+    let presupuestoAlmacenado = localStorage.getItem ("presupuesto")
+    if (presupuestoAlmacenado != null){
+        presupuestos = JSON.parse(presupuestoAlmacenado)
+        presupuestos = presupuestos.map ((presupuesto) => new Presupuesto (presupuesto.nombre, presupuesto.alto, presupuesto.ancho, presupuesto.profundidad))
+    }
+}
+function validarFormulario (event) {
+    event.preventDefault()
+    let nombre = inputNombre.value
+    let alto = parseFloat(inputAlto.value)
+    let ancho = parseFloat(inputAncho.value)
+    let profundidad = parseFloat(inputProfundidad.value)
+    let material = inputMaterial.value
+    let presupuesto = new Presupuesto(nombre, alto, ancho, profundidad, material)
+    presupuestos.push(presupuesto)
+    formulario.reset()   
+    calcularPresupuesto()
+    almacenarPresupuestoLocalStorage()
+}
+
+function calcularPresupuesto () {
+    contenedorPresupuesto.innerHTML =""
+    presupuestos.forEach ((presupuesto) =>{
+        let columna = document.createElement("div")
+        columna.innerHTML =`
+            <div class="card">
+                <div class="card_body">
+                    <p class="card_text">Presupuesto para: ${presupuesto.nombre}</p>
+                    <p class="card_text">Las dimensiones de su mueble son: ${presupuesto.alto}cm * ${presupuesto.ancho}cm * ${presupuesto.profundidad}cm  </p>
+                    <p class="card_text">Costo total: ${presupuesto.costoTotal()}  </p>
+                    <p class="card_text"></p>
+                    <p class="card_text"></p>
+                    <p class="card_text">Material: ${presupuesto.material}</p>      
+                </div>
+            </div>
+            `
+        contenedorPresupuesto.append (columna)
+    })
+}
+
+function main () {
+    inicializarElementos()
+    inicializarEventos()
+    obtenerPresupuestoLocalStorage()
+    calcularPresupuesto()
+}
+main()
